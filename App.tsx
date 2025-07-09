7a38878767208639ece3acc856b69b9e49d78f14
ruagiathi <ruagiathi@gmail.com>
2025-07-09 21:59:07 +0800

    video 29

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // data-style
+  // string 
+  const [name, setName] = useState<string>("ruagiathi");
+
+  // number
+  const [age, setAge] = useState<number>(45);
+
+  // null , undefined, boolean
+  const test = null;
+
+  //object array
+  const [person, setPerson] = useState({
+    name: "ruagiathi",
+    age: 45
+  });
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>Test</Text>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{JSON.stringify(person)}</Text>
       </View>
       <Text style={styles.text}>Hello World to
         <Text style={styles.ruagiathi}> Rua Gia Thi</Text>
@@ -26,7 +45,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    fontSize: 60, color: "red",
+    paddingTop: 20,
+    paddingHorizontal: 20,
   },
 });
